e24b75b3630a234ffacc3dc0b2fdf7d433f5729a
kieuishere <dvkieu165@gmail.com>
2024-04-14 08:38:18 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+const App = () => {
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello World! Kieu ne</p>
+                <div>Count = {count}</div>
+                <button onClick={() => dispatch(increaseCounter())}>
+                    Increase
+                </button>
+                <button onClick={() => dispatch(decreaseCounter())}>
+                    Decrease
+                </button>
+            </header>
+        </div>
+    );
+};
+
+export default App;
