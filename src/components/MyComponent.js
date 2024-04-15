e06747b6e1d4ecc038c15cf0d72fdb32c64b0a5a
kieuishere <dvkieu165@gmail.com>
2024-04-15 20:31:35 +0700

    #19 DOM events

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,20 @@ class MyComponent extends React.Component {
         age: 21,
     };
 
+    handleClick(event) {
+        console.log('click me my button');
+        console.log('My name is ', this.state.name);
+    }
+    handleOnMoverOver(event) {
+        console.log(event);
+    }
     //JSX : chỉ có React có. Cho phép code JS ngay trong HTML
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handleClick}>Click me!</button>
+                <button onMouseOver={this.handleOnMoverOver}>Hover me!</button>
             </div>
         );
     }
