7babe2b8df360cbde3987d1adce3b5d2912d0ba9
Duong Van Kieu <dvkieu165@gmail.com>
2024-05-04 10:08:51 +0700

    #16 Set up Boostrap

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="app-container">
-                Hello world
-                <MyComponent></MyComponent>
-            </div>
-        );
-    }
-}
-
-// const App = () => {
-//     const count = useSelector((state) => state.counter.count);
-//     const dispatch = useDispatch();
-
-//     return (
-//         <div className="App">
-//             <header className="App-header">
-//                 <img src={logo} className="App-logo" alt="logo" />
-//                 <p>Hello World! Kieu ne</p>
-//                 <div>Count = {count}</div>
-//                 <button onClick={() => dispatch(increaseCounter())}>
-//                     Increase
-//                 </button>
-//                 <button onClick={() => dispatch(decreaseCounter())}>
-//                     Decrease
-//                 </button>
-//             </header>
-//         </div>
-//     );
-// };
+const App = () => {
+    return <div className="app-container">Hello World</div>;
+};
 
 export default App;
