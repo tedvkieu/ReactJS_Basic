f71f2cdcdb426a6ce3b81672307a4d8b008906dd
Duong Van Kieu <dvkieu165@gmail.com>
2024-05-04 14:29:02 +0700

    #49 Nested Routes

--- src/index.js
+++ src/index.js
@@ -15,9 +15,10 @@ root.render(
         {/* <React.StrictMode> */}
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />}></Route>
-                <Route path="users" element={<User />}></Route>
-                <Route path="admin" element={<Admin />}></Route>
+                <Route path="/" element={<App />}>
+                    <Route path="/users" element={<User />}></Route>
+                    <Route path="/admin" element={<Admin />}></Route>
+                </Route>
             </Routes>
         </BrowserRouter>
 
