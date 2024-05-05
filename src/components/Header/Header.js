34d8de738f7d97ad5ff4b2f5c2ffe733fcf22b56
Duong Van Kieu <dvkieu165@gmail.com>
2024-05-05 08:52:05 +0700

    #50 Active Link - NavLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -12,15 +12,15 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/" className="nav-link">
+                        <NavLink to="/" className="nav-link">
                             Home
-                        </Link>
-                        <Link to="/users" className="nav-link">
+                        </NavLink>
+                        <NavLink to="/users" className="nav-link">
                             Users
-                        </Link>
-                        <Link to="/admin" className="nav-link">
+                        </NavLink>
+                        <NavLink to="/admin" className="nav-link">
                             Admin
-                        </Link>
+                        </NavLink>
                         {/* <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link href="/users">Users</Nav.Link>
                         <Nav.Link href="/admin">Admin</Nav.Link> */}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import User from './components/User/User';
 import Admin from './components/Admin/Admin';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
