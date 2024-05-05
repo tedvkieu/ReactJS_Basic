46bc03e91be598a676089bb491cf03326e592705
Duong Van Kieu <dvkieu165@gmail.com>
2024-05-05 13:43:45 +0700

    #52 Design HomePage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,12 @@
+import videoHomePage from '../../assets/video-homepage.mp4';
 const HomePage = (props) => {
-    return <div className="homepage-container">Home Page Component</div>;
+    return (
+        <div className="homepage-container">
+            <video autoPlay muted loop>
+                <source src={videoHomePage} type="video/mp4" />
+            </video>
+        </div>
+    );
 };
 
 export default HomePage;
