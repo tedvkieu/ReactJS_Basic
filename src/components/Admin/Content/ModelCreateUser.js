335543086517f063bae173fa4b1db6954e9a8ee6
Duong Van Kieu <dvkieu165@gmail.com>
2024-05-10 20:45:36 +0700

    #60 API Services

--- src/components/Admin/Content/ModelCreateUser.js
+++ src/components/Admin/Content/ModelCreateUser.js
@@ -1,10 +1,9 @@
-import { eventWrapper } from '@testing-library/user-event/dist/utils';
-import axios from 'axios';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
 import { toast } from 'react-toastify';
+import { postCreateNewUser } from '../../../services/apiServices';
 
 const ModelCreateUser = (props) => {
     const { show, setShow } = props;
@@ -55,25 +54,21 @@ const ModelCreateUser = (props) => {
             return;
         }
 
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
-
-        let res = await axios.post(
-            'http://localhost:8081/api/v1/participant',
-            data
+        let data = await postCreateNewUser(
+            email,
+            password,
+            username,
+            role,
+            image
         );
-        console.log('check res: ', res.data);
-        if (res.data && res.data.EC === 0) {
-            toast.success(res.data.EM);
+        console.log('check res: ', data);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
             handleClose();
         }
 
-        if (res.data && res.data.EC !== 0) {
-            toast.error(res.data.EM);
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
         }
     };
 
---
+++ src/services/apiServices.js
@@ -0,0 +1,14 @@
+import axios from '../utils/axiosCustomize';
+
+const postCreateNewUser = (email, password, username, role, image) => {
+    const data = new FormData();
+    data.append('email', email);
+    data.append('password', password);
+    data.append('username', username);
+    data.append('role', role);
+    data.append('userImage', image);
+
+    return axios.post('api/v1/participant', data); // da lay duong link local ben file instance
+};
+
+export { postCreateNewUser };
