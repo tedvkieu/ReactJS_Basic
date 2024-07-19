8c972bee8164c8a1bd770c7dafce5fc45eb48b85
tedvkieu <dvkieu165@gmail.com>
2024-07-19 19:50:22 +0700

    #80. Redux persist - Xử lý Data Khi F5 Refresh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -13,9 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         {/* <React.StrictMode> */}
-        <BrowserRouter>
-            <Layout />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+                <Layout />
+            </BrowserRouter>
+        </PersistGate>
 
         {/* </React.StrictMode> */}
     </Provider>
